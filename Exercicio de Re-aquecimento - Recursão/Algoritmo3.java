49a50fe6b4473d730201cedfc813b8fb789973f6
Lucas <oliveira.lucas23@edu.pucrs.br>
2025-02-26 22:02:39 -0300

    ajuste besta

--- Exercicio de Re-aquecimento - Recursão/Algoritmo3.java
+++ Exercicio de Re-aquecimento - Recursão/Algoritmo3.java
@@ -1,12 +1,12 @@
 public class Algoritmo3 {
-    public static double calculo(int n){
+    public static double calculo(double n){
         if(n == 1) {
             return 1;
         }
-        return calculo(n- 1) + 1.0/n;
+        return calculo(n- 1) + 1/n;
     }
 
     public static void main(String[] args) {
-        System.out.println(calculo(2));
+        System.out.println(calculo(5));
     }
 }
